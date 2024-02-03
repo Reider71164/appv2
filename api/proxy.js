3bb566b2544f8c7de162c563d39afd797ee635c7
Reider71164 <91398971+Reider71164@users.noreply.github.com>
2024-02-02 21:49:09 -0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,26 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = (req, res) => {
-  let target = "https://open.spotify.com"; // Default target
+const targets = {
+  default: "https://www.spotify.com",
+  specificPrefix: "http://106.15.2.32:6969",
+  accounts: "https://accounts.spotify.com/",
+  open: "https://open.spotify.com",
+  // Add more targets as needed
+};
 
-  if (
-    req.url.startsWith("/api") ||
-    req.url.startsWith("/auth") ||
-    req.url.startsWith("/banner") ||
-    req.url.startsWith("/CollegeTask")
-  ) {
-    target = "http://106.15.2.32:6969"; // Change target for specific URL prefixes
+module.exports = (req, res) => {
+  let target = targets.default; // Default target
+
+  if (req.url.startsWith("/api") || req.url.startsWith("/auth") || req.url.startsWith("/banner") || req.url.startsWith("/CollegeTask")) {
+    target = targets.specificPrefix; // Change target for specific URL prefixes
+  } else if (req.url.startsWith("/signup")) {
+    target = targets.signUp; // Change target for sign up requests
+  } else if (req.url.startsWith("/open")) {
+    target = targets.open; // Change target for "/open" requests
   }
+
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Target: ${target}`);
 
   createProxyMiddleware({
     target,
