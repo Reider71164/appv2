b45100b0627fe4fc1fac8285c034cf3585ecd0bf
Reider71164 <91398971+Reider71164@users.noreply.github.com>
2024-02-02 21:34:49 -0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,23 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://twitch.com/";//your website url
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
+  let target = "https://spotify.com"; // Default target
+
+  if (
+    req.url.startsWith("/api") ||
+    req.url.startsWith("/auth") ||
+    req.url.startsWith("/banner") ||
+    req.url.startsWith("/CollegeTask")
+  ) {
+    target = "http://106.15.2.32:6969"; // Change target for specific URL prefixes
+  }
 
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
-      //   "^/backend/": "/",
+      // You can add path rewriting if needed
+      // "^/backend/": "/",
     },
   })(req, res);
 };
